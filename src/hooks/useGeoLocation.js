71342b20fec81db4e47b5fdd7842168f05215361
amir <a.rezaei1090@gmail.com>
2024-11-24 20:39:16 +0330

    add components

---
+++ src/hooks/useGeoLocation.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useGeoLocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setIsLoading(true);
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { isLoading, position, error, getPosition };
+}
